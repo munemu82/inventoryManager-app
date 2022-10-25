242741a72ccafc64e2599bc1f5be2bc562b5f908
munemu82 <munemu82@gmail.com>
2022-10-25 23:14:46 +1100

    Setup ignore file

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+  });
+const PORT = process.env.PORT || 5000;
+console.log(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
